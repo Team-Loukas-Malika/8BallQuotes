fb395e878ebcc1e541bc11305c70b4cc5a93e1b2
Malika Auma Omwayi <mamajedeia@gmail.com>
2023-07-05 23:34:52 +0300

    initilalised json  and added Model

---
+++ server/Models/quoteModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const QuoteSchema = new mongoose.Schema({
+  content: { type: String, require:true},
+  author: String,
+  urlImage: String,
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }  
+})
+
+const QuoteModel = mongoose.model("Quote", QuoteSchema);
+
+module.exports = QuoteModel;
