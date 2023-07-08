db5d0a7b676845f586765327a270f47c3351bf2a
Malika Auma Omwayi <mamajedeia@gmail.com>
2023-07-08 20:36:25 +0300

    qball added

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -10,7 +10,7 @@ function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        <Navbar.Brand href="#">8ballQuotes</Navbar.Brand>
+        <Navbar.Brand href="/">8ballQuotes</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Form className="d-flex me-auto">
@@ -20,7 +20,7 @@ function Header() {
               className="me-2"
               aria-label="Search"
             />
-            <Button variant="outline-success"><Nav.Link href="/">Search</Nav.Link>Search</Button>
+            <Button variant="outline-success">Search</Button>
           </Form>
           <Nav className="my-2 my-lg-0" navbarScroll>
             <Button variant="outline-primary" className="mx-2"><Nav.Link href="/createq"  >CreateQ</Nav.Link></Button>
