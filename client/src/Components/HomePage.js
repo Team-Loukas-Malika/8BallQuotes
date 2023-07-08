c1394fe3ed67d5a2fe467a6207586146bd939f11
Malika Auma Omwayi <mamajedeia@gmail.com>
2023-07-08 20:49:39 +0300

    qball functionality added

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -2,24 +2,12 @@ import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import "./style.css"
 import QuoteOfDay from "./QuoteOfDay";
-import qball from "../assets/images/Qball.png"
+
 
 function HomePage() {
-    var navigate = useNavigate();
-
-    function createq(){
-        navigate ("/createq");
-      }
-
-    function qlibrary(){
-        navigate ("/qlibrary");
-      }
-
-
-
+    
     return (
         <div className="home-page">
-          <img src={qball} alt="qball" /> 
           <QuoteOfDay/>
             
         </div>
